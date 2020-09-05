c2fa16d7da49c0a71beec2318073168056a6627a
Jacob Bond <jacobpbond4@gmail.com>
2020-09-05 18:52:17 +0100

    Hard coded :|

--- pages/oauth/index.vue
+++ pages/oauth/index.vue
@@ -5,7 +5,7 @@
 <script>
 export default {
   mounted () {
-    window.location = 'https://discord.com/api/oauth2/authorize?client_id=682258887440597022&prompt=none&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&response_type=code&scope=identify%20guilds'
+    window.location = 'https://discord.com/api/oauth2/authorize?client_id=640989075452723200&redirect_uri=https%3A%2F%2Fdiscordfeeds.com%2Fcallback&response_type=code&scope=guilds%20identify'
   }
 }
 </script>
