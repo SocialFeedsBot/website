dd473cd5197b9b12dee197295cbfe9b7caf36145
jacob <jacobpbond4@gmail.com>
2020-12-18 20:54:12 +0000

    Add applications.commands scope

--- pages/invite/index.vue
+++ pages/invite/index.vue
@@ -5,7 +5,7 @@
 <script>
 export default {
   mounted () {
-    window.location = 'https://discord.com/api/oauth2/authorize?client_id=640989075452723200&permissions=0&scope=bot'
+    window.location = 'https://discord.com/api/oauth2/authorize?client_id=640989075452723200&permissions=0&scope=bot%20applications.commands'
   }
 }
 </script>
