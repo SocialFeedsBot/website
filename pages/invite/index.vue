e0fe3a2deac2fb675c32dbce35202b0a2033dba5
Jacob Bond <jacobpbond4@gmail.com>
2021-08-31 15:59:39 +0100

    feat: perms in invite link

--- pages/invite/index.vue
+++ pages/invite/index.vue
@@ -7,7 +7,7 @@ import config from '../../public_config.json'
 
 export default {
   mounted () {
-    window.location = `https://discord.com/api/oauth2/authorize?client_id=${config.clientID}&permissions=0&scope=bot%20applications.commands`
+    window.location = `https://discord.com/api/oauth2/authorize?client_id=${config.clientID}&permissions=53687091&scope=bot%20applications.commands`
   }
 }
 </script>
