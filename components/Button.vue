94c00860d86e99dec2875630a64bd46443bdb37f
getaddrinfo <oss@getaddrinfo.com>
2022-04-13 18:42:06 +0100

    feat: Button

---
+++ components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="button">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/css/_variables.scss";
+
+.button {
+    padding: 0.75rem 1.5rem;
+    border: none;
+    background-color: $blurple;
+    border-radius: 0.5rem;
+
+    font-family: $font-family-brand;
+    font-weight: 600;
+    font-size: 1rem;
+    color: #fff;
+}
+</style>
